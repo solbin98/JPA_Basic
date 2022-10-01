b4a07aa220745930781b4c5669efce7e0de07363
solbin98 <76681977+solbin98@users.noreply.github.com>
2022-10-01 19:32:49 +0900

    학습 2일차 진도 ~ 25%

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -12,8 +12,30 @@ public class JpaMain {
 
         tx.begin();
 
-        List<Member> result = em.createQuery("select m from Member as m", Member.class).getResultList();
+        //비영속
+        //Member member = new Member();
+        //member.setId(101L);
+        //member.setName("abcd");
 
+        //영속
+        //System.out.println("---- before ----");
+        //em.persist(member);
+        //System.out.println("---- after ----");
+
+        //Member findMember = em.find(Member.class, 101L);
+        //System.out.println("id : " + findMember.getId());
+        //System.out.println("name : " + findMember.getName());
+
+        // Member findMember1 = em.find(Member.class, 101L);
+        // Member findMember2 = em.find(Member.class, 101L); 29 번째 줄에서 context 에 가져오므로 캐시로 접근해서 가져옴.
+
+
+        // 영속
+        //Member mem1 = new Member(150L, "a1");
+        //Member mem2 = new Member(160L, "a2");
+        //em.persist(mem1);
+        //em.persist(mem2);
+        //System.out.println("---- here ---- ");
         tx.commit();
 
         //code
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -3,9 +3,8 @@ package hellojpa;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 @Getter
@@ -13,5 +12,28 @@ import javax.persistence.Id;
 public class Member {
     @Id
     private Long id;
+    @Column(name = "name")
     private String name;
+
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createDats;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiredDate;
+
+    @Lob
+    private String description;
+
+    @Transient
+    private int tmp;
+
+    public Member(long l, String a1) {
+        this.id = l;
+        this.name = a1;
+    }
+
+    public Member() {}
 }
